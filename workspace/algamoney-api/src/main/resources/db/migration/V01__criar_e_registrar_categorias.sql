0759515d18b6313879324ddb4f83f1d76bd60ea8
Nivaldo Noga <nnoga@brq.com>
2018-01-31 11:36:10 -0200

    ajustes scripts base

--- workspace/algamoney-api/src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
+++ workspace/algamoney-api/src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -3,8 +3,3 @@ CREATE TABLE categoria (
 	nome VARCHAR(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO categoria (nome) values ('Lazer');
-INSERT INTO categoria (nome) values ('Alimentacao');
-INSERT INTO categoria (nome) values ('Supermercado');
-INSERT INTO categoria (nome) values ('Farmacia');
-INSERT INTO categoria (nome) values ('Outros');
