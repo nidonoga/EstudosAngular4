bebf5be8e4824dce55b9f1791dea08ff8395944a
Nivaldo Noga <nnoga@brq.com>
2018-02-07 17:00:00 -0200

    Implementando autenticação Basic

--- workspace/algamoney-api/src/main/java/com/example/algamoney/api/resorce/LancamentoResource.java
+++ workspace/algamoney-api/src/main/java/com/example/algamoney/api/resorce/LancamentoResource.java
@@ -11,14 +11,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.algamoney.api.event.RecursoCriadoEvent;
@@ -46,8 +50,8 @@ public class LancamentoResource {
 	private MessageSource messageSource;
 	
 	@GetMapping
-	public List<Lancamento> pesquisar(LancamentoFilter lancamentoFilter){
-		return lancamentoRepository.filtrar(lancamentoFilter);
+	public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable){
+		return lancamentoRepository.filtrar(lancamentoFilter, pageable);
 	}
 	
 	
@@ -73,5 +77,12 @@ public class LancamentoResource {
 		List<CustomizedErrorMessage> erros = Arrays.asList(new CustomizedErrorMessage(mensagemUsuario, mensagemDev));
 		return ResponseEntity.badRequest().body(erros);
 	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		lancamentoRepository.delete(codigo);
+	}
+	
 
 }
